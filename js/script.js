06096e045c3215b14687b104694aacf52c807a73
Zoommie <zdinhsd@gmail.com>
2021-02-17 23:13:25 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+//STEP 1 
+
+//let someMonth;
+//function theMonth(){};
+//let currentMonth;
+//let summerMonth;
+//let myLibraryFunction;
+
+//STEP 2 
+
+//let age = 18;
+//let string = "Goodness";
+//let yes = true;
+//let x = null;
+
+//STEP 3
+
+//let average = 100/20;
+//let x = (100 + 20 ) * average;
+
+//STEP 4
+
+//let firstName;
+//let lastName;
+//let Address;
+//let City;
+//let State;
+//let zipCode;
+//let yourAge;
+//let referralSource
+//let mayWeContactYou
+
+//STEP 5
+
+//let firstName = Zung;
+//let lastName = Dinh;
+//let zipCode = 92111; 
+
+//STEP 6 
+
+//let legalAge = (21 + " age" ); console.log(legalAge);
+
+//STEP 7
+
+//let yes = prompt(true, "yes" );
+//let no = prompt(8,false );
+
+//STEP 8
+//let someString = " Who once said" + "," + " Only two things are infinite, the universe and human stupidity, and I'm not sure about the former. "; console.log(someString);
+
+//STEP 9
+//let none = null; console.log(none);
+//let object = undefined; console.log(object);
+
+
+//STEP 10
+
+//console.log(typeof 'Dude');
+//console.log(typeof 18);//
+//console.log(typeof true);
+//console.log(typeof {date:3});
+//console.log(typeof undeclaredVariable);
+
+//STEP 11
+//alert (" Hello " + " Zung Dinh, " + " welcome to the JavaScript class! ");
+
+//STEP 12
+ //let name = "Zung"; 
+ //alert(name);
+
+//STEP 13
+
+ //let course = "Javascript"; 
+ //alert(course);
+
+//STEP 14
+
+//let quotes = alert("Hello Zung Dinh \nWelcome to the JavaScript class");
+
+//STEP 15
+
+//let name = "Zung";
+//name = "What's your name";
+//prompt(name);
+
+//STEP 16
+
+//let course = "Javascript";
+//course = "What class you taking?";
+//prompt(course);
+
+ //STEP 17
+
+ //let x = 10;
+ //let y = 20;
+ //console.log(x + y);
+
+ //STEP 18
+
+//let x = 20;
+//x = 20+20;
+//console.log(x);
+
+//STEP 19
+
+//let x = 20;
+//x = x * 5;
+//console.log(x);
+
+//STEP 20
+
+//let x = 20%3;
+//x = x/1;
+//console.log(x);
+
+//STEP 21
+
+//let x = 7;
+//if ( x < 11){
+// console.log("this is number is right");
+//};
+
+//STEP 22
+
+//let x = 7;
+//if ( x > 11){
+//console.log("this is number is right");
+//}else{
+//console.log("this is false");
+//};
+
+
+
